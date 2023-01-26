b328975c71b2265e4ccc9324f0a6df1fb003d6d2
Laurence James-Woodley <35935416+ljwoodley@users.noreply.github.com>
2023-01-25 21:46:36 -0500

    test2

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@ drop table if exists dbo.test_query;
 select 
 	Retailer,
 	12 as UnitsSold,
-  'git_test' as source_col
+  'git2_test' as source_col
 into test_query
 from [dbo].[Sales_LOAD]
 where SalesValueUsd > 5000
